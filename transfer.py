a025caa76def175d703cfdf23e5be05b27e7ceeb
KTC9_gewei <grey@pku.edu.cn>
2024-09-04 14:14:29 +0800

    optimize help info

--- transfer.py
+++ transfer.py
@@ -7,11 +7,11 @@ import shutil
 from zipfile import ZipFile
 
 
-LENGTH_TYPE = "i"
+LENGTH_TYPE = "i"  # int32
 HEADER_SIZE = struct.calcsize(LENGTH_TYPE)
 
 DATA_IS_READ = struct.pack(LENGTH_TYPE, 0)
-DATE_EOF = struct.pack(LENGTH_TYPE, -1)
+DATA_EOF = struct.pack(LENGTH_TYPE, -1)
 
 DATA_SIZE = 10 * 1024 * 1024  # 10 MB
 SEG_SIZE = HEADER_SIZE + DATA_SIZE
@@ -32,7 +32,7 @@ def file2shm(infile: str):
             while shm.buf[:HEADER_SIZE] != DATA_IS_READ:
                 time.sleep(1)
                 print("waiting")
-    shm.buf[:HEADER_SIZE] = DATE_EOF
+    shm.buf[:HEADER_SIZE] = DATA_EOF
 
     try:
         while True:
@@ -47,7 +47,7 @@ def file2shm(infile: str):
 def shm2file(outfile: str):
     shm = SharedMemory(name="shm_test", create=False, size=SEG_SIZE)
     with open(outfile, "wb") as file:
-        while shm.buf[:HEADER_SIZE] != DATE_EOF:
+        while shm.buf[:HEADER_SIZE] != DATA_EOF:
             if shm.buf[:HEADER_SIZE] != DATA_IS_READ:
                 real_len = struct.unpack(LENGTH_TYPE, shm.buf[:HEADER_SIZE])[0]
                 file.write(shm.buf[HEADER_SIZE : HEADER_SIZE + real_len])
@@ -64,7 +64,7 @@ def process(args):
             outfile = f"{args.out}/output.zip"
             shm2file(outfile)
         else:
-            print("-out must specifiy a directory")
+            print("-o must specifiy a directory")
     elif args.command == "w":
         if os.path.isfile(args.input):
             ZipFile("tmp.zip", mode="w").write(args.input)  # without compression
@@ -73,18 +73,18 @@ def process(args):
             shutil.make_archive("tmp", format="zip", root_dir=args.input)
             file2shm("tmp.zip")
         else:
-            print("-input must specify filename or dirname")
+            print("-i must specify filename or dirname")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="transfer file by shared memory")
+    parser = argparse.ArgumentParser(description="transfer file by sharing memory")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
-    parser_a = subparsers.add_parser("r", help="reader, read from shared memory")
-    parser_a.add_argument("-out", type=str, default=".", help="output directory")
+    parser_w = subparsers.add_parser("w", help="writer, file to shm, run first")
+    parser_w.add_argument("-i", dest="input", type=str, required=True, help="the input singlefile or directory")
 
-    parser_b = subparsers.add_parser("w", help="writer, write to shared memory")
-    parser_b.add_argument("-input", type=str, required=True, help="the input singlefile or directory")
+    parser_r = subparsers.add_parser("r", help="reader, shm to file, run second")
+    parser_r.add_argument("-o", dest="out", type=str, default=".", help="output directory")
 
     args = parser.parse_args()
     process(args)
